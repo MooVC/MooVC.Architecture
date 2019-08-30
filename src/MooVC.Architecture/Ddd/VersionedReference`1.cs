d4f5397848160ac9bba8a49be108f4a743793bd4
MooVC <vmartins.paul@gmail.com>
2019-08-30 10:43:55 +0100

    Added a shortcut for VersionedReference to Reference

--- src/MooVC.Architecture/Ddd/VersionedReference`1.cs
+++ src/MooVC.Architecture/Ddd/VersionedReference`1.cs
@@ -13,6 +13,8 @@ namespace MooVC.Architecture.Ddd
         private static readonly Lazy<VersionedReference<TAggregate>> ActualEmpty =
             new Lazy<VersionedReference<TAggregate>>(() => new VersionedReference<TAggregate>());
 
+        private readonly Lazy<Reference<TAggregate>> actualReference;
+
         public VersionedReference(Guid id, ulong version = AggregateRoot.DefaultVersion)
             : base(id, version)
         {
@@ -21,24 +23,31 @@ namespace MooVC.Architecture.Ddd
                 nameof(id),
                 value => value != Guid.Empty,
                 GenericIdInvalid);
+
+            actualReference = new Lazy<Reference<TAggregate>>(() => this.ToReference());
         }
 
         public VersionedReference(TAggregate aggregate)
             : base(aggregate)
         {
+            actualReference = new Lazy<Reference<TAggregate>>(() => this.ToReference());
         }
 
         private VersionedReference()
             : base(Guid.Empty, 0)
         {
+            actualReference = new Lazy<Reference<TAggregate>>(() => Reference<TAggregate>.Empty);
         }
 
         private VersionedReference(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
+            actualReference = new Lazy<Reference<TAggregate>>(() => this.ToReference());
         }
 
         public static VersionedReference<TAggregate> Empty => ActualEmpty.Value;
+
+        public Reference<TAggregate> Reference => actualReference.Value;
 
         public override Type Type => typeof(TAggregate);
     }

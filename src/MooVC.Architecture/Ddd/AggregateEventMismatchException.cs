c35097a0ac702ea9afbecf738761702195c6c77e
MooVC <vmartins.paul@gmail.com>
2020-01-23 10:41:11 +0000

    AggregateEventMismatchException no longer requires an aggregate name +semver:breaking

--- src/MooVC.Architecture/Ddd/AggregateEventMismatchException.cs
+++ src/MooVC.Architecture/Ddd/AggregateEventMismatchException.cs
@@ -7,7 +7,7 @@
     public sealed class AggregateEventMismatchException
         : ArgumentException
     {
-        public AggregateEventMismatchException(string aggregateName, VersionedReference aggregate, VersionedReference eventAggregate)
+        public AggregateEventMismatchException(VersionedReference aggregate, VersionedReference eventAggregate)
             : base(string.Format(
                 AggregateEventMismatchExceptionMessage,
                 aggregate.Id,
@@ -18,13 +18,10 @@
                 eventAggregate.Version))
         {
             Aggregate = aggregate;
-            AggregateName = aggregateName;
             EventAggregate = eventAggregate;
         }
 
         public VersionedReference Aggregate { get; }
-
-        public string AggregateName { get; }
 
         public VersionedReference EventAggregate { get; }
     }

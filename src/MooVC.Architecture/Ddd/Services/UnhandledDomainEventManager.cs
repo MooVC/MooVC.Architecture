3b2ccf8a5003374facddb1c9c0a90a9d8a386ab5
MooVC <vmartins.paul@gmail.com>
2020-02-15 13:02:03 +0000

    Changed DelayedStartSaga to inherit from TimedJobQueue Added a new UnhandledDomainEventManager, designed to handle retry for events that failed to be handled by an IBus (Domain Event) implementation

---
+++ src/MooVC.Architecture/Ddd/Services/UnhandledDomainEventManager.cs
@@ -0,0 +1,67 @@
+﻿namespace MooVC.Architecture.Ddd.Services
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using MooVC.Collections.Generic;
+    using MooVC.Logging;
+    using MooVC.Processing;
+
+    public sealed class UnhandledDomainEventManager
+        : TimedJobQueue<DomainEventUnhandledEventArgs>,
+          IEmitFailures
+    {
+        private readonly IBus bus;
+
+        public UnhandledDomainEventManager(IBus bus, TimedProcessor timer)
+            : base(timer)
+        {
+            this.bus = bus;
+
+            this.bus.Unhandled += Bus_Unhandled;
+        }
+
+        public event PassiveExceptionEventHandler FailureEmitted;
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                bus.Unhandled -= Bus_Unhandled;
+
+                base.Dispose(isDisposing);
+            }
+        }
+
+        protected override void OnFailureEncountered(Exception failure)
+        {
+            FailureEmitted?.Invoke(this, new PassiveExceptionEventArgs(failure));
+        }
+
+        protected override IEnumerable<DomainEventUnhandledEventArgs> Process(IEnumerable<DomainEventUnhandledEventArgs> jobs)
+        {
+            var failures = new ConcurrentBag<DomainEventUnhandledEventArgs>();
+
+            jobs.ForAll(job =>
+            {
+                try
+                {
+                    job.Handler();
+                }
+                catch (Exception ex)
+                {
+                    OnFailureEncountered(ex);
+
+                    failures.Add(job);
+                }
+            });
+
+            return failures;
+        }
+
+        private void Bus_Unhandled(IBus sender, DomainEventUnhandledEventArgs e)
+        {
+            Enqueue(e);
+        }
+    }
+}

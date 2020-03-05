c72c421cff6e042172ddb462761eeb1a73b6295f
MooVC <vmartins.paul@gmail.com>
2020-03-05 09:48:21 +0000

    Added an extension that will create a reference from a Guid

---
+++ src/MooVC.Architecture.Tests/Ddd/GuidExtensionsTests/WhenToReferenceIsCalled.cs
@@ -0,0 +1,26 @@
+﻿namespace MooVC.Architecture.Ddd.GuidExtensionsTests
+{
+    using System;
+    using Xunit;
+
+    public sealed class WhenToReferenceIsCalled
+    {
+        [Fact]
+        public void GivenAnEmptyGuidThenTheEmptyReferenceIsReturned()
+        {
+            var reference = Guid.Empty.ToReference<AggregateRoot>();
+
+            Assert.Same(Reference<AggregateRoot>.Empty, reference);
+        }
+
+        [Fact]
+        public void GivenANonEmptyGuidThenAReferenceWithThatIdIsReturned()
+        {
+            var id = Guid.NewGuid();
+            var reference = id.ToReference<AggregateRoot>();
+
+            Assert.NotEqual(Reference<AggregateRoot>.Empty, reference);
+            Assert.Equal(id, reference.Id);
+        }
+    }
+}

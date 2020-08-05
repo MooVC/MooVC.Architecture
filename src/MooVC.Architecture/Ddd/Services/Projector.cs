4aa192b4c9232f8dee1ce31b4ec2348d63c0ef72
Paul Martins <pmartins@controlsoft.ie>
2020-08-05 09:48:40 +0100

    Added a base projector

---
+++ src/MooVC.Architecture/Ddd/Services/Projector.cs
@@ -0,0 +1,18 @@
+﻿namespace MooVC.Architecture.Ddd.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public abstract class Projector<TAggregate, TProjection>
+        : IProjector<TAggregate, TProjection>
+        where TAggregate : AggregateRoot
+        where TProjection : Projection<TAggregate>
+    {
+        public TProjection Project(TAggregate aggregate)
+        {
+            return Project(new[] { aggregate }).Single();
+        }
+
+        public abstract IEnumerable<TProjection> Project(IEnumerable<TAggregate> aggregates);
+    }
+}

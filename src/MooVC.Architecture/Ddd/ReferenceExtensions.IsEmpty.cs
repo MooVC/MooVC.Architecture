277c4dcffec5c9f6a6b8ed913c0de7bf967dd94b
Paul Martins <pmartins@controlsoft.ie>
2021-09-09 09:23:20 +0100

    - Changed Ddd.ReferenceExtensions.IsEmpty so that the input is flagged as not null when the return value is false.

--- src/MooVC.Architecture/Ddd/ReferenceExtensions.IsEmpty.cs
+++ src/MooVC.Architecture/Ddd/ReferenceExtensions.IsEmpty.cs
@@ -1,8 +1,10 @@
 ﻿namespace MooVC.Architecture.Ddd
 {
+    using System.Diagnostics.CodeAnalysis;
+
     public static partial class ReferenceExtensions
     {
-        public static bool IsEmpty(this Reference? reference)
+        public static bool IsEmpty([NotNullWhen(false)] this Reference? reference)
         {
             return reference?.IsEmpty ?? true;
         }
